0161f8ea1453ba33b2a963c4b0ce21159c0de9e5
d4rknorris <marcosfelipedomborovski@gmail.com>
2024-11-11 21:10:10 -0300

    detalhe

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,10 +1,12 @@
 import {historiaType} from './historiaType'
 import { categoria } from './categoriaType'
 import { servicosType } from './servicosType'
+import { cuidadoType } from './cuidadoType'
 
 export const schemaTypes = 
 [
     historiaType,
     categoria,
-    servicosType
+    servicosType,
+    cuidadoType    
 ]
